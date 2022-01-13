0aeb15973af500281e423901b83c3980b4b59f79
driuft <air.socarras@gmail.com>
2022-01-13 00:26:36 -0500

    Added logic to calculate Total based on tip amount

--- Pro-Tip/ViewController.swift
+++ Pro-Tip/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        billAmountTextField.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
+                
+        // show keyboard
+        billAmountTextField.becomeFirstResponder()
     }
 
-
+    @IBAction func calculateTip(_ sender: Any) {
+        // Get bill amount from text field input
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        // Get Total tip by multiplying tip * tipPercentage
+        let tipPercentages = [0.15, 0.18, 0.2]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        // Update Tip Amount Label
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        
+        // Update Total Amount
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    @IBAction func textFieldChanged(_ sender: UITextView) {
+        calculateTip(sender)
+    }
 }
-
